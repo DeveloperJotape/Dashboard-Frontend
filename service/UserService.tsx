3aed0605cf7607ce13dfd09aa929fc1ed01cf2ca
developerjotape <developerjotape@gmail.com>
2025-01-06 14:34:10 -0300

    perf: Refatoração de código

--- service/UserService.tsx
+++ service/UserService.tsx
@@ -1,13 +1,7 @@
-import axios from "axios";
-import { BaseService } from "./BaseService";
-
-export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/user'
-});
+import { BaseService } from './BaseService';
 
 export class UserService extends BaseService {
-
     constructor() {
-        super("/user")
+        super('/user');
     }
 }
